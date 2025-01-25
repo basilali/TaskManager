7e2bf636250c43b504499fe4b8fdfbdc67303433
basilali <basil.ali@mohawkcollege.ca>
2025-01-25 01:35:01 -0500

    Added Logic

--- Main.rb
+++ Main.rb
@@ -4,18 +4,26 @@ tasks = []
 
 while true do
   puts "What would you like to do? "
-  puts "1. View tasks - 2. Create new task - 3. Quit: "
+  puts "1. View tasks - 2. Create new task - 3. Save Tasks - 4. Load Tasks: "
   choice = gets.chomp
 
+  # Need to break into methods
   case choice
   when "1"
     if tasks.length == 0
       puts "No tasks found"
     else
-      puts "|\t Name \t|\t Description \t|\t Deadline \t|\t Priority \t|\t Status \t|"
-      tasks.each do  |task|
-        puts "|\t #{task.name} \t|\t #{task.description} \t|\t #{task.deadline} \t|\t #{task.priority} \t|\t #{task.priority} \t|"
+      header = ["Name", "Description", "Deadline", "Priority", "Status"]
+
+      puts "+------------+-----------------------+------------+----------+-----------+"
+      puts "| %-10s | %-21s | %-10s | %-8s | %-9s |" % header
+      puts "+------------+-----------------------+------------+----------+-----------+"
+
+      tasks.each do |row|
+        row = row.to_array
+        puts "| %-10s | %-21s | %-10s | %-8s | %-9s |" % row
       end
+      puts "+------------+-----------------------+------------+----------+-----------+"
 
     end
     
@@ -28,12 +36,45 @@ while true do
     puts "Enter deadline:"
     task_deadline = gets.chomp
     puts "Choose priority 1(low)-3(high):"
-    task_priority = gets.chomp
+    input = gets.chomp.to_i
+    until input.between?(1, 3)
+        puts "Please choose a valid priority"
+        puts "Choose priority 1(low)-3(high):"
+        input = gets.chomp.to_i
+    end
+    task_priority = input
 
     tasks << Task.new(task_name, task_description, task_deadline, task_priority)
 
   when "3"
-    break
+
+    if !tasks.length == 0
+      data = []
+      tasks.each do |task|
+        data << task.to_array
+      end
+
+      File.open("tasks.txt", "w") do |f|
+        tasks.each do |task|
+        f.puts task.to_array.join(",")
+        end
+      end
+
+      puts "Tasks saved!"
+    else
+      puts "No tasks to be saved!"
+    end
+
+  when "4"
+
+    File.readlines("tasks.txt").each do |line|
+      line = line.strip
+      data = line.split(",")
+      tasks << Task.new(data[0], data[1], data[2], data[3], data[4])
+    end
+
+    puts "Tasks loaded!"
+
   else
     puts "Invalid entry, please try again"
   end
--- Task.rb
+++ Task.rb
@@ -1,12 +1,23 @@
 class Task
   attr_accessor :name, :description, :status, :priority, :deadline
 
-  def initialize(name, description, deadline, priority)
+  def initialize(name, description, deadline, priority, status="Pending")
     @name = name
     @description = description
     @deadline = deadline
     @priority = priority
-    @status = "Open"
+    @status = status
+  end
+
+  def to_array
+    [@name, @description, @deadline, @priority, @status]
+  end
+
+  def in_progress
+    @status == "In Progress"
+  end
+  def done
+    @status = "Done"
   end
 
 end
